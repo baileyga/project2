45e44af1a5a1acd2f35d61afd0661ab7261d8acb
Alex Buchanan <buchanaj@dukes.jmu.edu>
2022-04-11 15:14:16 -0400

    Update signInOut.js

--- JS/signInOut.js
+++ JS/signInOut.js
@@ -1,11 +1,20 @@
 var signedIn = false;
-var signInButton = document.getElementById("Login");
+var isAdmin = false;
+var username = '';
+
+var signInButton = document.getElementById("Login"); 
 var logOutButton = document.getElementById("logOut");
 
-signInButton.onclick = signIn() {
+signInButton.onclick = signIn(username,password); {
+    if(username == 'admin' & password == 'admin'); {
+        isAdmin = true;
+    }
+    username = '';
     signedIn=true;
 }
 
-logOutButton.onclick = log_Out() {
+logOutButton.onclick = log_Out(); {
+    username = '';
     signedIn=false;
+    isAdmin=false;
 }
