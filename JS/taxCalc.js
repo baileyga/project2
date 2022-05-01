544df891b50a68c6a21310c96054767a67d3456e
baileyga <baileyga@dukes.jmu.edu>
2022-05-01 15:26:33 -0400

    edited taxCalc.js

--- JS/taxCalc.js
+++ JS/taxCalc.js
@@ -30,21 +30,3 @@ var calculate_tax = function() {
     */
 }
 }
-window.onload = function () {
-    $("max2colors").onclick = calculate_tax;
-}
-
-var incomeEl = document.getElementById('income')
-var wealthEl = document.getElementById('wealth')
-var taxEl = document.getElementById('tax');
-
-function calculate() {  
-  var incomeTax = 0.35 * incomeEl.value;
-  var wealthTax = 0.25 * wealthEl.value;
-  var tax =  incomeTax + wealthTax;
-
-  taxEl.value = Math.round(tax * 100) / 100;
-}
-
-incomeEl.addEventListener('input', calculate);
-wealthEl.addEventListener('input', calculate);
