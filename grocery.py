3f27ba0fdb381864ccef8da2d3c279c5ca8bde19
Alex Buchanan <buchanaj@dukes.jmu.edu>
2022-05-01 20:19:31 -0400

    grocery start items

---
+++ grocery.py
@@ -0,0 +1,52 @@
+# Alex Buchanan
+
+from asyncio.windows_events import NULL
+from flask import Flask, redirect, render_template, request
+import psycopg
+
+app = Flask(__name__)
+
+sql = """
+SELECT item, amount,price
+FROM grocery;
+"""
+
+
+@app.route('/')
+def select():
+    con = psycopg.connect("host=localhost dbname=sec1 user=buchanaj password=111948864")
+    cur = con.cursor()
+    cur.execute(sql)
+    data = cur.fetchall()
+
+    return render_template("grocery.html", data=data)
+
+newCar = """
+INSERT INTO grocery
+VALUES (%s, %s,%s);
+"""
+
+@app.route('/ins', methods=['POST'])
+def insert():
+    vin = request.form.get('item','')
+    make = request.form.get('amount','')
+    price = request.form.get('price','')
+    con = psycopg.connect("host=localhost dbname=sec1 user=buchanaj password=111948864")
+    cur = con.cursor()
+    cur.execute(newCar, (vin,make,price))
+    con.commit()
+    return redirect('/')
+
+removeItem = """
+DELETE FROM grocery
+where vin = %s;
+"""
+
+@app.route('/del/<item>')
+def delete(item):
+    con = psycopg.connect("host=localhost dbname=sec1 user=buchanaj password=111948864")
+    cur = con.cursor()
+    cur.execute(removeItem, (item,))
+    con.commit()
+    return redirect('/')
+    
